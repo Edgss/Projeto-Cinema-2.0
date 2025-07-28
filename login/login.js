5314c00539fc409b650108ff90d628b941ef5f50
Edgss <eduardoogomess211@gmail.com>
2025-07-28 17:03:02 -0300

    login completo

--- login/login.js
+++ login/login.js
@@ -1,3 +1,5 @@
+//Visualizar senha
+
 let senha = document.getElementById("senha")
 let icone = document.getElementById("icone")
 
@@ -9,3 +11,28 @@ function verSenha(){
     icone.classList.toggle("bi-eye-fill")
     icone.classList.toggle("bi-eye-slash")
 }
+
+
+//Modo Claro
+let trilho = document.getElementById("trilho")
+let banner = document.getElementById("banner")
+let fundo = document.getElementById("fundo")
+const body = document.querySelector("body")
+
+
+trilho.addEventListener("click", ()=>{
+    trilho.classList.toggle("dark")
+    body.classList.toggle("dark")
+    
+
+    if(banner.src.includes("users.png")){
+        banner.src = "../imgs/users-white.png"
+        body.style.backgroundImage = "url(../imgs/backgroundWhite2.png"
+    }
+    else{
+        banner.src = "../imgs/users.png"
+        body.style.backgroundImage = "url(../imgs/background.png)"
+    }
+
+
+})
